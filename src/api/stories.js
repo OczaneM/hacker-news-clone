44c488312c61b572f3af861ff48ccc4f59f91bec
OczaneM <oczane.rivera@gmail.com>
2022-11-24 22:12:13 -0500

    fetch individual stories

--- src/api/stories.js
+++ src/api/stories.js
@@ -6,10 +6,19 @@ const newStories = `${baseUrl}/newstories.json`
 
 export const getNewStories = async () => {
   try {
-    const newStoriesRes = await axios
-      .get(newStories)
-      .then((response) => response.data)
+    const newStoriesRes = await axios.get(newStories).then((res) => res.data)
     return newStoriesRes
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getStory = async (id) => {
+  try {
+    const story = await axios
+      .get(`${baseUrl}/item/${id}.json?print=pretty`)
+      .then((res) => res.data)
+    return story
   } catch (error) {
     console.error(error)
   }
